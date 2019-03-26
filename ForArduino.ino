7c217b58b68776933a92579d7dff6a0bacd61951
PorYoung <PorYoung@users.noreply.github.com>
2019-03-26 17:44:16 +0800

    Update ForArduino.ino

--- ForArduino.ino
+++ ForArduino.ino
@@ -13,8 +13,13 @@ int value = 0;
 long lastRecv = 0;
 long lastTime = 0;
 bool newDataComing = false;
+String JsonDataTemplator = "{\"TH2O\":\"TH2O_VALUE\",\"PHH2O\":\"PHH2O_VALUE\",\"TDS\":\"TDS_VALUE\",\"ORP\":\"ORP_VALUE\",\"LEVEL\":\"LEVEL_VALUE\",\"TAIR\":\"TAIR_VALUE\",\"LIGHT\":\"LIGHT_VALUE\",\"RHSUB\":\"RHSUB_VALUE\",\"RHAIR\":\"RHAIR_VALUE\"}";
 
-float getTemp() {
+String strRecv = "";
+bool mqttConnected = false;
+bool stopUploadAllData = false;
+
+float getTH2OTemp() {
   sensors.requestTemperatures(); // Send the command to get temperatures
   //  Serial.print("Temperature for Device 1 is: ");
   float temp = sensors.getTempCByIndex(0);
@@ -29,7 +34,6 @@ void setup() {
   espSerial.begin(9600);
   sensors.begin();
 }
-String strRecv = "";
 void loop() {
   // put your main code here, to run repeatedly:
   long now = millis();
@@ -43,17 +47,35 @@ void loop() {
     debugSerial.print("[Receive:");
     debugSerial.println(strRecv);
     debugSerial.println("]");
+    if (strRecv.equals("-1")) {
+      mqttConnected = false;
+    } else {
+      mqttConnected = true;
+      if (strRecv.indexOf("|1|") != -1 && !stopUploadAllData) {
+        stopUploadAllData = true;
+      } else if (strRecv.indexOf("|2|") != -1 && stopUploadAllData) {
+        stopUploadAllData = false;
+      }
+    }
     strRecv = "";
     newDataComing = false;
   }
-  if (now - lastTime > 1000) {
+  if (now - lastTime > 5000 && mqttConnected && !stopUploadAllData) {
     lastTime = now;
-    float temp = getTemp();
-    String JsonData = "{\"Temp\":\"TEMP_VALUE\"}";
-    JsonData.replace("TEMP_VALUE", String(temp));
+    float TH2OTemp = getTH2OTemp();
+    String JsonData = JsonDataTemplator;
+    JsonData.replace("TH2O_VALUE", String(TH2OTemp));
+    JsonData.replace("PHH2O_VALUE", String(0));
+    JsonData.replace("ORP_VALUE", String(0));
+    JsonData.replace("LEVEL_VALUE", String(0));
+    JsonData.replace("TAIR_VALUE", String(0));
+    JsonData.replace("LIGHT_VALUE", String(0));
+    JsonData.replace("RHSUB_VALUE", String(0));
+    JsonData.replace("RHAIR_VALUE", String(0));
     debugSerial.println("[Send:");
     debugSerial.println(JsonData);
     debugSerial.println("]");
     Serial.println(JsonData);
+    Serial.flush();
   }
 }
